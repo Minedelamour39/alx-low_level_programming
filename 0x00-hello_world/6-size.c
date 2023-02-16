cdf950d1ed148c7706ae33a13388c332aa3cc2f4
Mine <ilyadelamour@gmail.com>
2023-02-16 08:10:27 -0800

    101-quote.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * Return: always 0(Success)
+ * /
+
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a char: %lu byte(s)\n", sizeof(char)); 
+printf("Size of an int:%lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 #ifdef __x86_64__
-
+printf("Size of a pointer: %lu byte(s)\n", sizeof(void *));
+printf("Size of a long double: %lu byte(s)\n", sizeof(long double));
+#else
+printf("Size of a pointer: %lu byte(s)\n", sizeof(void *));
+#endif
 return (0);
 }
 
