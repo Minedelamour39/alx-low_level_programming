599a0b10466c3a495fb91c548a5676604d115ea3
Mine <ilyadelamour@gmail.com>
2023-02-16 08:23:14 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: always 0(Success)
- * /
 
+/**
+ * main - Prints the size of various types on the computer it is compiled on.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char)); 
-printf("Size of an int:%lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
-#ifdef __x86_64__
-printf("Size of a pointer: %lu byte(s)\n", sizeof(void *));
-printf("Size of a long double: %lu byte(s)\n", sizeof(long double));
-#else
-printf("Size of a pointer: %lu byte(s)\n", sizeof(void *));
-#endif
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
 
