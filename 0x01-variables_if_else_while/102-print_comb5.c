569f71d8395385ce4954662519cf8f6a2b69d7bd
Mine <ilyadelamour@gmail.com>
2023-02-17 06:45:26 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,34 +7,34 @@
  */
 int main(void)
 {
-	int a, b, c, d;
+int a, b, c, d;
 
-	for (a = 0; a < 100; a++)
-	{
-		for (b = 0; b < 100; b++)
-		{
-			c = a / 10;
-			d = a % 10;
+for (a = 0; a < 100; a++)
+{
+for (b = 0; b < 100; b++)
+{
+c = a / 10;
+d = a % 10;
 
-			if (b > a)
-			{
-				c = b / 10;
-				d = b % 10;
+if (b > a)
+{
+c = b / 10;
+d = b % 10;
 
-				putchar((a / 10) + '0');
-				putchar((a % 10) + '0');
-				putchar(' ');
-				putchar((b / 10) + '0');
-				putchar((b % 10) + '0');
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
+putchar(' ');
+putchar((b / 10) + '0');
+putchar((b % 10) + '0');
 
-				if (a < 98 || b < 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+if (a < 98 || b < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
