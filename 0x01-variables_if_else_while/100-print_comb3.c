5a46c8b225f66249374ebe6cb9af267f4b02bf18
Mine <ilyadelamour@gmail.com>
2023-02-17 06:39:29 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,27 @@
-#include<tsdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: Prints all possible combinations of single-digit numbers
- * separated by a comma and a space, in ascending order.
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int i;
+int i, j;
 
-for (i = 0; i < 10; i++)
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
 {
 putchar(i + '0');
-
-if (i != 9)
+putchar(j + '0');
+if (i < 8)
 {
 putchar(',');
 putchar(' ');
 }
 }
-
+}
 putchar('\n');
 
 return (0);
