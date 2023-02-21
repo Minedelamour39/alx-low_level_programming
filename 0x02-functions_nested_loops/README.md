0747bdf2b137f79963da79b4b4cb1d8ff90e598b
Mine <ilyadelamour@gmail.com>
2023-02-21 04:56:33 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,13 @@
+# Project Title
+
+Print _putchar Program
+
+## Description
+
+This project includes a program that prints `_putchar`, followed by a new line.
+
+## Getting Started
+
+To compile the program, run the following command in the terminal:
+
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,7 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
-void  print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int 3-islower.c
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
+int _putchar(char c);
 
+#endif /* MAIN_H */
 
-#endif
