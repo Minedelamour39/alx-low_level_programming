694b7a05018ca7b1aaa7ba88e13e097b7953285e
Mine <ilyadelamour@gmail.com>
2023-02-22 04:04:14 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,36 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers.
+ * main - Prints the first 98 Fibonacci numbers
  *
- * Return: Always 0.
+ * Return: Always 0
  */
+
 int main(void)
 {
-unsigned long int i, j, k;
-unsigned long int j_prev, k_prev, overflow;
+int i;
+unsigned long int fib1 = 1, fib2 = 2, fib3;
 
-i = 1;
-j = 2;
-j_prev = 1;
-k_prev = 2;
-printf("%lu, %lu", i, j);
+printf("%lu, %lu", fib1, fib2);
 
-for (int n = 3; n <= 98; n++)
+for (i = 2; i < 98; i++)
 {
-if (k_prev > 0)
-overflow = (k / 1000000000) + (j / 1000000000);
-else
-overflow = 0;
-
-k = (k_prev + j_prev) % 1000000000;
-j = (j_prev + k_prev + overflow) % 1000000000;
-j_prev = k_prev;
-k_prev = k;
-
-printf(", %lu", j);
+fib3 = fib1 + fib2;
+printf(", %lu", fib3);
+fib1 = fib2;
+fib2 = fib3;
 }
 
-putchar('\n');
+printf("\n");
+
 return (0);
 }
