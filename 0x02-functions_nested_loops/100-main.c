783e7692df173ed1a44231056fcc00757b0a2fad
Mine <ilyadelamour@gmail.com>
2023-02-22 04:16:51 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -14,6 +14,5 @@ _putchar('\n');
 print_times_table(98);
 _putchar('\n');
 print_times_table(12);
-
 return (0);
 }
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,25 +2,47 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: the size of the table
+ * @n: integer number
  */
 void print_times_table(int n)
 {
-if (n < 0 || n > 15)
-return;
+	int i, j, res;
 
-int i, j, res;
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				res = i * j;
+				if (j == 0)
+					_putchar('0' + res);
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (res < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + res);
+					}
+					else if (res < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + res / 10);
+						_putchar('0' + res % 10);
+					}
+					else
+					{
+						_putchar('0' + res / 100);
+						_putchar('0' + (res / 10) % 10);
+						_putchar('0' + res % 10);
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
 
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-res = i * j;
-if (j == 0)
-printf("%d", res);
-else
-printf(", %d", res);
-}
-printf("\n");
-}
-}
