8678f439d577b2c260a269855f36bb88f2219d54
Mine <ilyadelamour@gmail.com>
2023-02-22 03:06:30 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+long int fib1 = 1, fib2 = 2, fib3;
+
+printf("%ld, %ld", fib1, fib2);
+
+for (i = 0; i < 48; i++)
+{
+fib3 = fib1 + fib2;
+printf(", %ld", fib3);
+fib1 = fib2;
+fib2 = fib3;
+}
+
+printf("\n");
+return (0);
+}
+
