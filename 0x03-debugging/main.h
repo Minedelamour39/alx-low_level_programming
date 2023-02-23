7ab7611808323a4696bf6ecbf7a996db43211c1a
Mine <ilyadelamour@gmail.com>
2023-02-23 10:01:57 -0800

    1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,13 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 
-#endif /* MAIN_H */
+#endif /* MAIN_H*/
