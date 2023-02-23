cc644d44b69499c37f5e36c464e5fd63ca568826
Mine <ilyadelamour@gmail.com>
2023-02-23 09:29:06 -0800

    1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-void positive_or_negative(int i);
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+int main(void);
 
 #endif /* MAIN_H */
